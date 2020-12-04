9e7404b1aeed15b0b45788f99a39ba7e632bee6c
ParkJongSang <whdtkd546@gmail.com>
2020-12-04 18:39:36 +0900

    [DAYOL] Main.storyboard 제거

--- Dayol/Dayol/ViewController.swift
+++ Dayol/Dayol/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	let helloDayolLabel: UILabel = {
+		let label = UILabel()
+		label.text = "Hello, Dayol"
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.textColor = .black
+		label.textAlignment = .center
+		label.sizeToFit()
+
+		return label
+	}()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		view.backgroundColor = .white
+		view.addSubview(helloDayolLabel)
+		helloDayolLabel.center = view.center
 	}
-
-
 }
 
