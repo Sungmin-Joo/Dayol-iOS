81c0387abe5c06e768e34dfdcfe674cbd093370d
Sungmin-Joo <big-joo_dev@naver.com>
2020-12-19 03:07:18 +0900

    HomeTabBar 구현

---
+++ Dayol-iOS/UI/Home/View/HomeTabBarButton.swift
@@ -0,0 +1,116 @@
+//
+//  HomeTabBarButton.swift
+//  Dayol-iOS
+//
+//  Created by Sungmin on 2020/12/17.
+//
+
+import RxSwift
+
+class HomeTabBarButton: UIButton {
+
+    enum Style {
+        case diary
+        case favorite
+    }
+
+    let style: HomeTabBarButton.Style
+    private(set) var selectedImage: UIImage?
+    private(set) var normalImage: UIImage?
+    private(set) var normalTitle: NSAttributedString?
+    private(set) var selectedTitle: NSAttributedString?
+
+    override var isSelected: Bool {
+        didSet {
+            updateButtonState()
+        }
+    }
+
+    init(style: HomeTabBarButton.Style) {
+        self.style = style
+        super.init(frame: .zero)
+        setupButtonStyle()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func updateButtonState() {
+        let image = isSelected ? selectedImage : normalImage
+        let title = isSelected ? selectedTitle : normalTitle
+        let isUserfaceEnable = (isSelected == false)
+
+        setImage(image, for: .normal)
+        setAttributedTitle(title, for: .normal)
+        isUserInteractionEnabled = isUserfaceEnable
+    }
+
+}
+
+extension HomeTabBarButton {
+
+    private func setupButtonStyle() {
+        guard style == .diary else {
+            setupFavoriteButton()
+            return
+        }
+        setupDiaryButton()
+    }
+
+    private func setupDiaryButton() {
+        normalImage = Design.diaryNormal
+        selectedImage = Design.diarySelected
+        normalTitle = Design.buttonTitle(text: Design.diaryText, false)
+        selectedTitle = Design.buttonTitle(text: Design.diaryText, true)
+    }
+
+    private func setupFavoriteButton() {
+        normalImage = Design.favoriteNormal
+        selectedImage = Design.favoriteSelected
+        normalTitle = Design.buttonTitle(text: Design.favoriteText, false)
+        selectedTitle = Design.buttonTitle(text: Design.favoriteText, true)
+    }
+
+}
+
+private enum Design {
+    // Text
+    static let diaryText = "다이어리"
+    static let favoriteText = "즐겨찾기"
+
+    // Attributed Text
+    static let buttonParagraphStyle: NSMutableParagraphStyle = {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.center
+        return paragraphStyle
+    }()
+    static let buttonTitleAttributes: [NSAttributedString.Key: Any] = {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .paragraphStyle: buttonParagraphStyle,
+            .font: buttonTitleFont,
+            .kern: -0.3
+        ]
+        return attributes
+    }()
+    static func buttonTitle(text: String, _ isSelected: Bool) -> NSAttributedString {
+        let fgColor = isSelected ? buttonSelectedFGColor : buttonDeselectedFGColor
+        let attributedText = NSMutableAttributedString(string: text,
+                                                       attributes: buttonTitleAttributes)
+        attributedText.addAttribute(.foregroundColor,
+                                    value: fgColor,
+                                    range: NSRange(location: 0, length: text.count))
+
+        return attributedText
+    }
+    // Font
+    static let buttonTitleFont = UIFont.appleBold(size: 16.0)
+    // Image
+    static let diaryNormal = Assets.Image.Home.TabBarDiary.normal
+    static let diarySelected = Assets.Image.Home.TabBarDiary.selected
+    static let favoriteNormal = Assets.Image.Home.TabBarFavorite.normal
+    static let favoriteSelected = Assets.Image.Home.TabBarFavorite.selected
+    // Color
+    static let buttonSelectedFGColor = UIColor.black
+    static let buttonDeselectedFGColor = UIColor.black.withAlphaComponent(0.3)
+}
---
+++ Dayol-iOS/UI/Home/View/HomeTabBarView.swift
@@ -0,0 +1,166 @@
+//
+//  HomeTabBarView.swift
+//  Dayol-iOS
+//
+//  Created by Sungmin on 2020/12/16.
+//
+
+import RxCocoa
+import RxSwift
+
+class HomeTabBarView: UIView {
+
+    enum TabType {
+        case diary
+        case favorite
+    }
+
+    enum EventType{
+        case diary
+        case plusButton
+        case favorite
+    }
+
+    let buttonEvent = PublishSubject<HomeTabBarView.EventType>()
+    let disposeBag = DisposeBag()
+
+    private let plusButton: UIButton = {
+        let button = UIButton()
+        button.setImage(Design.plusButton, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    private let diaryButton = HomeTabBarButton(style: .diary)
+    private let favoriteButton = HomeTabBarButton(style: .favorite)
+    private(set) var currentTabMode: TabType {
+        didSet {
+            updateTabBarState()
+        }
+    }
+
+    init(mode: TabType) {
+        self.currentTabMode = mode
+        super.init(frame: .zero)
+        setupViews()
+        setupContentsLayout()
+        bindEvent()
+        updateTabBarState()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+// MARK: - Event
+extension HomeTabBarView {
+    private func bindEvent() {
+        plusButton.rx.tap.bind { [weak self] in
+            self?.buttonEvent.onNext(.plusButton)
+        }
+        .disposed(by: disposeBag)
+
+        diaryButton.rx.tap.bind { [weak self] in
+            self?.currentTabMode = .diary
+            self?.buttonEvent.onNext(.diary)
+        }
+        .disposed(by: disposeBag)
+
+        favoriteButton.rx.tap.bind { [weak self] in
+            self?.currentTabMode = .favorite
+            self?.buttonEvent.onNext(.favorite)
+        }
+        .disposed(by: disposeBag)
+    }
+
+    private func updateTabBarState() {
+        guard currentTabMode == .diary else {
+            diaryButton.isSelected = false
+            favoriteButton.isSelected = true
+            return
+        }
+        diaryButton.isSelected = true
+        favoriteButton.isSelected = false
+    }
+
+}
+
+// MARK: - Setup
+extension HomeTabBarView {
+    private func setupViews() {
+        backgroundColor = Design.safeAreaColor
+
+        addSubview(buttonStackView)
+        addSubview(plusButton)
+
+        buttonStackView.addArrangedSubview(diaryButton)
+        buttonStackView.addArrangedSubview(favoriteButton)
+
+        setupTopLineView()
+        setupSafeAreaView()
+    }
+
+    private func setupTopLineView() {
+        let lineView = UIView(frame: .zero)
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+        lineView.backgroundColor = Design.lineColor
+        buttonStackView.addSubview(lineView)
+
+        NSLayoutConstraint.activate([
+            lineView.topAnchor.constraint(equalTo: buttonStackView.topAnchor),
+            lineView.leadingAnchor.constraint(equalTo: buttonStackView.leadingAnchor),
+            lineView.trailingAnchor.constraint(equalTo: buttonStackView.trailingAnchor),
+            lineView.heightAnchor.constraint(equalToConstant: Design.lineViewHeight)
+        ])
+    }
+
+    private func setupSafeAreaView() {
+        let safeAreaView = UIView(frame: .zero)
+        safeAreaView.translatesAutoresizingMaskIntoConstraints = false
+        safeAreaView.backgroundColor = Design.safeAreaColor
+        addSubview(safeAreaView)
+
+        NSLayoutConstraint.activate([
+            safeAreaView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            safeAreaView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            safeAreaView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            safeAreaView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    private func setupContentsLayout() {
+        NSLayoutConstraint.activate([
+            buttonStackView.topAnchor.constraint(equalTo: topAnchor),
+            buttonStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            buttonStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            buttonStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            buttonStackView.heightAnchor.constraint(equalToConstant: Design.stackViewHeight),
+
+            plusButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            plusButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            plusButton.widthAnchor.constraint(equalToConstant: Design.plusButtonWidth),
+            plusButton.heightAnchor.constraint(equalToConstant: Design.plusButtonHeight)
+        ])
+    }
+}
+
+private enum Design {
+    // Layout
+    static let stackViewHeight: CGFloat = 55.0
+    static let plusButtonHeight: CGFloat = 70.0
+    static let plusButtonWidth: CGFloat = 70.0
+    static let lineViewHeight: CGFloat = 1.0
+    // Image
+    static let plusButton = Assets.Image.Home.plusButton
+    // Color
+    static let lineColor = UIColor(decimalRed: 233, green: 233, blue: 233)
+    static let safeAreaColor = UIColor.white
+}
