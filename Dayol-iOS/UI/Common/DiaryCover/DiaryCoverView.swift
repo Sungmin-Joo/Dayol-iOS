329e6b36bb71ae18f682f61a20cb826daa20e047
ParkJongSang <whdtkd546@gmail.com>
2020-12-26 13:32:34 +0900

    [Modify] Design Type

--- Dayol-iOS/UI/Common/DiaryCover/DiaryCoverView.swift
+++ Dayol-iOS/UI/Common/DiaryCover/DiaryCoverView.swift
@@ -8,40 +8,44 @@
 import UIKit
 
 private enum Design {
-	static func rightRadius(type: DiaryType) -> CGFloat {
-		switch type {
-		case .big: return 16
+    case big
+    case medium
+    case small
+    
+    var rightRadius: CGFloat {
+		switch self {
+        case .big: return 16
 		case .medium: return 8
 		case .small: return 2
 		}
 	}
 
-	static func leftRadius(type: DiaryType) -> CGFloat {
-		switch type {
+    var leftRadius: CGFloat {
+		switch self {
 		case .big: return 8
 		case .medium: return 4
 		case .small: return 1
 		}
 	}
 
-	static func lineWidth(type: DiaryType) -> CGFloat {
-		switch type {
+    var lineWidth: CGFloat {
+		switch self {
 		case .big: return 8
 		case .medium: return 4
 		case .small: return 2
 		}
 	}
 
-	static func leftViewWidth(type: DiaryType) -> CGFloat {
-		switch type {
+    var leftViewWidth: CGFloat {
+		switch self {
 		case .big: return 48
 		case .medium: return 24
 		case .small: return 6
 		}
 	}
 
-	static func rightViewWidth(type: DiaryType) -> CGFloat {
-		switch type {
+    var rightViewWidth: CGFloat {
+		switch self {
 		case .big: return 492
 		case .medium: return 246
 		case .small: return 246 / 4
@@ -52,7 +56,7 @@ private enum Design {
 }
 
 class DiaryCoverView: UIView {
-	let type: DiaryType
+	private let design: Design
 
 	private let stackView: UIStackView = {
 		let stackView = UIStackView()
@@ -86,7 +90,11 @@ class DiaryCoverView: UIView {
 	}()
 
 	init(type: DiaryType, backgroundColor: UIColor?) {
-		self.type = type
+        switch type {
+        case .big: self.design = .big
+        case .medium: self.design = .medium
+        case .small: self.design = .small
+        }
 		super.init(frame: .zero)
 		initView(backgroundColor: backgroundColor)
 	}
@@ -108,31 +116,30 @@ class DiaryCoverView: UIView {
 	private func setRightView(backgroundColor: UIColor?) {
 		rightView.backgroundColor = backgroundColor
 		rightView.clipsToBounds = true
-		rightView.layer.cornerRadius = Design.rightRadius(type: type)
+        rightView.layer.cornerRadius = design.rightRadius
 		rightView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
-
-		rightView.widthAnchor.constraint(equalToConstant: Design.rightViewWidth(type: type)).isActive = true
 	}
 
 	private func setLeftView(backgroundColor: UIColor?) {
 		leftView.backgroundColor = backgroundColor
 		leftView.clipsToBounds = true
-		leftView.layer.cornerRadius = Design.leftRadius(type: type)
+		leftView.layer.cornerRadius = design.leftRadius
 		leftView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
 		leftView.addSubview(coverLineView)
-
-		leftView.widthAnchor.constraint(equalToConstant: Design.leftViewWidth(type: type)).isActive = true
 	}
 
 	private func setConstraints() {
 		NSLayoutConstraint.activate([
+            rightView.widthAnchor.constraint(equalToConstant: design.rightViewWidth),
+            leftView.widthAnchor.constraint(equalToConstant: design.leftViewWidth),
+            
 			stackView.leftAnchor.constraint(equalTo: leftAnchor),
 			stackView.rightAnchor.constraint(equalTo: rightAnchor),
 			stackView.topAnchor.constraint(equalTo: topAnchor),
 			stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
 
 			coverLineView.rightAnchor.constraint(equalTo: leftView.rightAnchor),
-			coverLineView.widthAnchor.constraint(equalToConstant: Design.lineWidth(type: type)),
+            coverLineView.widthAnchor.constraint(equalToConstant: design.lineWidth),
 			coverLineView.topAnchor.constraint(equalTo: leftView.topAnchor),
 			coverLineView.bottomAnchor.constraint(equalTo: leftView.bottomAnchor)
 		])
--- Dayol-iOS/UI/Common/DiaryCover/DiaryLockerView.swift
+++ Dayol-iOS/UI/Common/DiaryCover/DiaryLockerView.swift
@@ -8,64 +8,68 @@
 import UIKit
 
 private enum Design {
-	static func lockerHeight(type: DiaryType) -> CGFloat {
-		switch type {
+    case big
+    case medium
+    case small
+    
+	var lockerHeight: CGFloat {
+		switch self {
 		case .big: return 120
 		case .medium: return 60
 		case .small: return 15
 		}
 	}
 
-	static func buttonInset(type: DiaryType) -> CGFloat {
-		switch type {
+	var buttonInset: CGFloat {
+		switch self {
 		case .big: return 30
 		case .medium: return 15
 		case .small: return 15 / 4
 		}
 	}
 
-	static func buttonWidth(type: DiaryType) -> CGFloat {
-		switch type {
+	var buttonWidth: CGFloat {
+		switch self {
 		case .big: return 4.0
 		case .medium: return 2.0
 		case .small: return 0.5
 		}
 	}
 
-	static func buttonRadius(type: DiaryType) -> CGFloat {
-		switch type {
+	var buttonRadius: CGFloat {
+		switch self {
 		case .big: return 32
 		case .medium: return 16
 		case .small: return 4
 		}
 	}
 
-	static func rightRadius(type: DiaryType) -> CGFloat {
-		switch type {
+	var rightRadius: CGFloat {
+		switch self {
 		case .big: return 8
 		case .medium: return 4
 		case .small: return 1
 		}
 	}
 
-	static func leftRadius(type: DiaryType) -> CGFloat {
-		switch type {
+	var leftRadius: CGFloat {
+		switch self {
 		case .big: return 60
 		case .medium: return 30
 		case .small: return 30 / 4
 		}
 	}
 
-	static func leftViewWidth(type: DiaryType) -> CGFloat {
-		switch type {
+	var leftViewWidth: CGFloat {
+		switch self {
 		case .big: return 120
 		case .medium: return 60
 		case .small: return 15
 		}
 	}
 
-	static func rightViewWidth(type: DiaryType) -> CGFloat {
-		switch type {
+	var rightViewWidth: CGFloat {
+		switch self {
 		case .big: return 20
 		case .medium: return 10
 		case .small: return 10 / 4
@@ -77,7 +81,7 @@ private enum Design {
 }
 
 class DiaryLockerView: UIView {
-	private let type: DiaryType
+	private let design: Design
 
 	private let stackView: UIStackView = {
 		let stackView = UIStackView()
@@ -119,7 +123,11 @@ class DiaryLockerView: UIView {
 	}()
 
 	init(type: DiaryType, backgroundColor: UIColor?) {
-		self.type = type
+        switch type {
+        case .big: self.design = .big
+        case .medium: self.design = .medium
+        case .small: self.design = .small
+        }
 		super.init(frame: .zero)
 		initView(backgroundColor: backgroundColor)
 	}
@@ -144,44 +152,43 @@ class DiaryLockerView: UIView {
 		leftView.addSubview(lockImage)
 		leftView.addSubview(buttonView)
 		leftView.clipsToBounds = true
-		leftView.layer.cornerRadius = Design.leftRadius(type: type)
+        leftView.layer.cornerRadius = design.leftRadius
 		leftView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
-
-		leftView.widthAnchor.constraint(equalToConstant: Design.leftViewWidth(type: type)).isActive = true
 	}
 
 	private func setRightView(backgroundColor: UIColor?) {
 		rightView.backgroundColor = backgroundColor
 		rightView.clipsToBounds = true
-		rightView.layer.cornerRadius = Design.rightRadius(type: type)
+        rightView.layer.cornerRadius = design.rightRadius
 		rightView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
-
-		rightView.widthAnchor.constraint(equalToConstant: Design.rightViewWidth(type: type)).isActive = true
 	}
 
 	private func setButtonView() {
-		buttonView.layer.borderWidth = Design.buttonWidth(type: type)
+        buttonView.layer.borderWidth = design.buttonWidth
+        buttonView.layer.cornerRadius = design.buttonRadius
 		buttonView.layer.borderColor = Design.buttonBorderColor
-		buttonView.layer.cornerRadius = Design.buttonRadius(type: type)
 		buttonView.layer.masksToBounds = true
 	}
 
 	private func setConstraints() {
 		NSLayoutConstraint.activate([
+            leftView.widthAnchor.constraint(equalToConstant: design.leftViewWidth),
+            rightView.widthAnchor.constraint(equalToConstant: design.rightViewWidth),
+            
 			stackView.topAnchor.constraint(equalTo: topAnchor),
 			stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
 			stackView.leftAnchor.constraint(equalTo: leftAnchor),
 			stackView.rightAnchor.constraint(equalTo: rightAnchor),
 
-			lockImage.topAnchor.constraint(equalTo: topAnchor, constant: Design.buttonInset(type: type)),
-			lockImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Design.buttonInset(type: type)),
-			lockImage.leftAnchor.constraint(equalTo: leftAnchor, constant: Design.buttonInset(type: type)),
-			lockImage.rightAnchor.constraint(equalTo: rightAnchor, constant: -Design.buttonInset(type: type)),
+			lockImage.topAnchor.constraint(equalTo: topAnchor, constant: design.buttonInset),
+			lockImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -design.buttonInset),
+			lockImage.leftAnchor.constraint(equalTo: leftAnchor, constant: design.buttonInset),
+			lockImage.rightAnchor.constraint(equalTo: rightAnchor, constant: -design.buttonInset),
 
-			buttonView.topAnchor.constraint(equalTo: topAnchor, constant: Design.buttonInset(type: type)),
-			buttonView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Design.buttonInset(type: type)),
-			buttonView.leftAnchor.constraint(equalTo: leftAnchor, constant: Design.buttonInset(type: type)),
-			buttonView.rightAnchor.constraint(equalTo: rightAnchor, constant: -Design.buttonInset(type: type))
+			buttonView.topAnchor.constraint(equalTo: topAnchor, constant: design.buttonInset),
+			buttonView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -design.buttonInset),
+			buttonView.leftAnchor.constraint(equalTo: leftAnchor, constant: design.buttonInset),
+			buttonView.rightAnchor.constraint(equalTo: rightAnchor, constant: -design.buttonInset)
 		])
 	}
 }
