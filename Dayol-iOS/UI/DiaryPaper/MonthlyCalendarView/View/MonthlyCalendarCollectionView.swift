dc529881c2e20f7d76335c41223ea738e38b7f7c
ParkJongSang <whdtkd546@gmail.com>
2021-01-25 23:44:45 +0900

    [Add] WeekDay View

--- Dayol-iOS/UI/DiaryPaper/MonthlyCalendarView/View/MonthlyCalendarCollectionView.swift
+++ Dayol-iOS/UI/DiaryPaper/MonthlyCalendarView/View/MonthlyCalendarCollectionView.swift
@@ -5,4 +5,79 @@
 //  Created by 박종상 on 2021/01/25.
 //
 
-import Foundation
+import UIKit
+
+private enum Design {
+    static let dayFont = UIFont.systemFont(ofSize: 11, weight: .bold)
+    static let sundayColor = UIColor(decimalRed: 211, green: 27, blue: 27)
+    static let saturdayColor = UIColor(decimalRed: 43, green: 81, blue: 206)
+    static let dayColor: UIColor = .black
+    static let dayLetterSpace: CGFloat = -0.42
+    static let weekDayHeight: CGFloat = 20
+}
+
+class MonthlyCalendarCollectionView: UIView {
+    private let weekDayView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+
+    private let dayLabels: [UILabel] = {
+        var labels = [UILabel]()
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font : Design.dayFont,
+            .kern : Design.dayLetterSpace
+        ]
+        WeekDay.allCases.forEach { weekday in
+            let dayColor: UIColor
+            let label = UILabel()
+            if weekday == .sunday {
+                dayColor = Design.sundayColor
+            } else if weekday == .saturday {
+                dayColor = Design.saturdayColor
+            } else {
+                dayColor = Design.dayColor
+            }
+            let attributedText = NSAttributedString(string: weekday.stringValue, attributes: attributes)
+            label.textColor = dayColor
+            label.attributedText = attributedText
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.textAlignment = .center
+            labels.append(label)
+        }
+        
+        return labels
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        initView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    private func initView() {
+        addSubview(weekDayView)
+        dayLabels.forEach { weekDayView.addArrangedSubview($0) }
+        setConstraint()
+    }
+
+    private func setConstraint() {
+        NSLayoutConstraint.activate([
+            weekDayView.topAnchor.constraint(equalTo: topAnchor),
+            weekDayView.leftAnchor.constraint(equalTo: leftAnchor),
+            weekDayView.rightAnchor.constraint(equalTo: rightAnchor),
+            weekDayView.heightAnchor.constraint(equalToConstant: Design.weekDayHeight)
+        ])
+    }
+}
--- Dayol-iOS/UI/DiaryPaper/MonthlyCalendarView/View/MonthlyCalendarView.swift
+++ Dayol-iOS/UI/DiaryPaper/MonthlyCalendarView/View/MonthlyCalendarView.swift
@@ -31,22 +31,64 @@ private enum Design {
             }
         }
     }
-}
-
-class MonthlyCalendarView: UIView {
-    private var iPadOrientation: Design.IPadOrientation {
-        if self.frame.size.width < self.frame.size.height {
-            return .portrait
-        } else {
-            return .landscape
+    
+    static func collectionLeft(orientation: IPadOrientation) -> CGFloat {
+        switch current {
+        case .iphone:
+            return 0
+        case .ipad:
+            if orientation == .portrait {
+                return 77
+            } else {
+                return 0
+            }
         }
     }
     
-    let headerView: MonthlyCalendarHeaderView = {
+    static func collectionRight(orientation: IPadOrientation) -> CGFloat {
+        switch current {
+        case .iphone:
+            return 0
+        case .ipad:
+            if orientation == .portrait {
+                return 77
+            } else {
+                return 0
+            }
+        }
+    }
+}
+
+class MonthlyCalendarView: UIView {
+    private var containerViewLeft = NSLayoutConstraint()
+    private var containerViewRight = NSLayoutConstraint()
+    private var iPadOrientation: Design.IPadOrientation {
+        if self.frame.size.width > self.frame.size.height {
+            return .landscape
+        } else {
+            return .portrait
+        }
+    }
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private let headerView: MonthlyCalendarHeaderView = {
         let header = MonthlyCalendarHeaderView(month: .january)
         header.translatesAutoresizingMaskIntoConstraints = false
         
         return header
+    }()
+    
+    private let collectionView: MonthlyCalendarCollectionView = {
+        let collectionView = MonthlyCalendarCollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
     }()
     
     init() {
@@ -58,17 +100,38 @@ class MonthlyCalendarView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateContainerView()
+    }
+    
     private func initView() {
-        addSubview(headerView)
+        addSubview(containerView)
+        containerView.addSubview(headerView)
+        containerView.addSubview(collectionView)
         setConstraint()
     }
     
     private func setConstraint() {
+        containerViewLeft = containerView.leftAnchor.constraint(equalTo: leftAnchor, constant: Design.collectionLeft(orientation: iPadOrientation))
+        containerViewRight = containerView.rightAnchor.constraint(equalTo: rightAnchor, constant: -Design.collectionRight(orientation: iPadOrientation))
+        
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: topAnchor),
-            headerView.leftAnchor.constraint(equalTo: leftAnchor),
-            headerView.rightAnchor.constraint(equalTo: rightAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: Design.headerHeight(orientation: iPadOrientation))
+            containerViewLeft, containerViewRight,
+            headerView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            headerView.leftAnchor.constraint(equalTo: containerView.leftAnchor),
+            headerView.rightAnchor.constraint(equalTo: containerView.rightAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: Design.headerHeight(orientation: iPadOrientation)),
+            
+            collectionView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: containerView.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: containerView.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
         ])
+    }
+    
+    private func updateContainerView() {
+        containerViewLeft.constant = Design.collectionLeft(orientation: iPadOrientation)
+        containerViewRight.constant = -Design.collectionRight(orientation: iPadOrientation)
     }
 }
