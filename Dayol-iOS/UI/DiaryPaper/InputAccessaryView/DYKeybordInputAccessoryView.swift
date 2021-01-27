036bac2cf6e73e749972912a19c2274a9bdf8ccf
Sungmin-Joo <big-joo_dev@naver.com>
2021-01-28 00:31:55 +0900

    - [Mod] frame을 따로 설정해주지 않아도 되도록 수정

--- Dayol-iOS/UI/DiaryPaper/InputAccessaryView/DYKeybordInputAccessoryView.swift
+++ Dayol-iOS/UI/DiaryPaper/InputAccessaryView/DYKeybordInputAccessoryView.swift
@@ -82,7 +82,7 @@ class DYKeybordInputAccessoryView: UIView {
 
     init(currentColor: UIColor = Design.defaultTextColor) {
         self.currentColor = currentColor
-        super.init(frame: .zero)
+        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 44))
         setupViews()
         setupConstraints()
         setupHorizontalSeparator()
