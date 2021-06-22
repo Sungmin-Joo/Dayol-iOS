b653154b283edb7515afe99fa4afda5c58c1f03a
Sungmin-Joo <big-joo_dev@naver.com>
2021-06-23 00:16:23 +0900

    - 리소스 이름 변경 (fontname에 맞춰서 이름 변경)

--- Dayol-iOS/UI/Common/View/ToolBarContent/TextStyle/TextStyleFontViewModel.swift
+++ Dayol-iOS/UI/Common/View/ToolBarContent/TextStyle/TextStyleFontViewModel.swift
@@ -11,15 +11,15 @@ import RxSwift
 extension TextStyleFontViewModel {
     enum Font: String, CaseIterable {
         case system
-        case nanumsquare
-        case nanumsquareround
-        case dahaeng
-        case baeeunhye
-        case himnaera
-        case ridibatang
-        case binggrae
-        case binggraesamanco
-        case gmarketSans
+        case nanumsquare = "NanumSquare"
+        case nanumsquareround = "NanumSquareRound"
+        case dahaeng = "NanumDaHaengCe"
+        case baeeunhye = "NanumBaeEunHyeCe"
+        case himnaera = "NanumHimNaeRaNeunMarBoDan"
+        case ridibatang = "RIDIBatang"
+        case binggrae = "Binggrae"
+        case binggraesamanco = "BinggraeSamanco"
+        case gmarketSans = "GmarketSansTTFMedium"
 
         var thumbnailName: String? {
             guard self != .system else { return nil }
