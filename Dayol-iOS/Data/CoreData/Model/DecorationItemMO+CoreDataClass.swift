cd2cae85c753f6341d604f34f1273ca21aa6e7f4
Sungmin-Joo <big-joo_dev@naver.com>
2021-06-30 02:23:16 +0900

    - MO class 내부 구현

--- Dayol-iOS/Data/CoreData/Model/DecorationItemMO+CoreDataClass.swift
+++ Dayol-iOS/Data/CoreData/Model/DecorationItemMO+CoreDataClass.swift
@@ -11,5 +11,27 @@ import CoreData
 
 @objc(DecorationItemMO)
 public class DecorationItemMO: NSManagedObject {
-    
+
+    func set(_ item: DecorationItem) {
+        self.id = item.id
+        self.parentId = item.parentId
+        self.width = item.width
+        self.height = item.height
+        self.x = item.x
+        self.y = item.y
+    }
+
+    func getItem() -> DecorationItem? {
+        guard
+            let id = id,
+            let parentId = parentId
+        else { return nil }
+
+        return DecorationItem(id: id,
+                              parentId: parentId,
+                              width: width,
+                              height: height,
+                              x: x,
+                              y: y)
+    }
 }
--- Dayol-iOS/Data/CoreData/Model/DecorationTextFieldItemMO+CoreDataClass.swift
+++ Dayol-iOS/Data/CoreData/Model/DecorationTextFieldItemMO+CoreDataClass.swift
@@ -12,4 +12,27 @@ import CoreData
 @objc(DecorationTextFieldItemMO)
 public class DecorationTextFieldItemMO: DecorationItemMO {
 
+    override func set(_ item: DecorationItem) {
+        super.set(item)
+
+        if let textFieldItem = item as? DecorationTextFieldItem {
+            self.textData = textFieldItem.textData
+        }
+    }
+
+    override func getItem() -> DecorationTextFieldItem? {
+        guard
+            let itemDAO = super.getItem(),
+            let textData = textData
+        else { return nil }
+
+        return DecorationTextFieldItem(id: itemDAO.id,
+                                       parentId: itemDAO.parentId,
+                                       width: width,
+                                       height: height,
+                                       x: x,
+                                       y: y,
+                                       textData: textData)
+    }
+    
 }
