9889085c10739410e686a46cd87468ff197327a2
ParkJongSang <whdtkd546@gmail.com>
2021-07-11 21:03:23 +0900

    create MonthlyTackerPaper

--- Dayol-iOS/UI/DiaryPaper/Papers/MujiPaper.swift
+++ Dayol-iOS/UI/DiaryPaper/Papers/MujiPaper.swift
@@ -5,9 +5,22 @@
 //  Created by 주성민 on 2021/01/20.
 //
 
-import RxSwift
+import UIKit
 
 class MujiPaper: BasePaper {
     override var identifier: String { MujiPaper.className }
+
+    private let mujiView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    override func configure(viewModel: PaperViewModel, orientation: Paper.PaperOrientation) {
+        super.configure(viewModel: viewModel, orientation: orientation)
+
+        contentView.addSubViewPinEdge(mujiView)
+    }
 }
 
